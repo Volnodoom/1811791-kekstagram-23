ea19b161f264510a6e65faa3b76e4357f98838d4
Alexander <Bdiyoz@mail.ru>
2021-07-22 04:11:12 +0400

    module7-task1 thumbnails creation

--- js/modules/thumbnails.js
+++ js/modules/thumbnails.js
@@ -1,17 +1,27 @@
-import { getData } from "./send-get-data.js";
+import { getData } from './send-get-data.js';
 
 
 const URL_FOR_DATA = 'https://23.javascript.pages.academy/kekstagram/data';
-const tempData = getData(URL_FOR_DATA).then((data) => data.slice(0, 1));
+
 
 const boxWithThumbnails = document.querySelector('.pictures');
 const referencePic = document.querySelector('#picture').content.querySelector('.picture');
-const urlPic = referencePic.querySelector('img [src=""]');
-const likesPic = referencePic.querySelector('.picture__likes');
-const commentsPic = referencePic.querySelector('.picture__comments');
 
-const fragment = document.createDocumentFragment();
+const creatThumbnails = (datum) => {
+  const picElement = referencePic.cloneNode(true);
 
+  const urlPic = referencePic.querySelector('img');
+  urlPic.setAttribute ('src', datum.url);
 
+  const likesPic = referencePic.querySelector('.picture__likes');
+  likesPic.textContent = datum.likes;
+
+  const commentsPic = referencePic.querySelector('.picture__comments');
+  commentsPic.textContent = datum.comments;
+
+  return boxWithThumbnails.appendChild(picElement);
+};
+
+const tempData = getData(URL_FOR_DATA).then((data) => data.slice(1, 20).forEach((datum) => creatThumbnails (datum)));
 console.log (tempData);
 
